a68e0c77bc6f9fc25c3838923c33a9ef61fd0bb2
PedroRockembach <rockpedro11@gmail.com>
2025-05-09 09:31:16 -0300

    Atualização Do 89

--- Desafio -- 89.py
+++ Desafio -- 89.py
@@ -4,5 +4,36 @@ import os
 #pergunte ao usuario se o mesmo gostaria de ver alguma nota separadamente
 
 os.system("cls")
-boletim = [[],[]]
+boletim = [[],[],[]] #Nome - Prova 1 - Prova 2
 
+while True:
+    
+    print("="*30)
+    
+    nome = str (input("Digite seu nome: "))
+    
+    prova_um = float (input("Digite a nota da primeira prova: "))
+    prova_dois = float (input("Digite a nota da segunda prova: "))
+    
+    boletim[0].append(nome)
+    boletim[1].append(prova_um)
+    boletim[2].append(prova_dois)
+    
+    saida = str(input("Desaja continuar? [S/N] ")).lower()
+    
+    if saida in "nnao":
+        
+        break
+  
+print("="*30)
+
+print(f"Boletim Escolar Semestral:\n{"nO":<3}{"Nome":>10}{"Nota":>14}")
+
+print("-"*30)
+
+for b in range(len(boletim[0])):
+    
+    print(f"{b:<4}{boletim[0]:>10}{(boletim[1]+boletim[2])/2:>12}")
+    
+print("="*30)
+    
