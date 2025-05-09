481e22240fe923b096313ba9c0ab8b840c9c0197
PedroRockembach <rockpedro11@gmail.com>
2025-05-09 09:45:04 -0300

    att
    verificação de caractere de saida e correção da media para não ser feita durante o for e sim dentro do while com as demais variaveis

--- Desafio -- 89.py
+++ Desafio -- 89.py
@@ -4,7 +4,7 @@ import os
 #pergunte ao usuario se o mesmo gostaria de ver alguma nota separadamente
 
 os.system("cls")
-boletim = [[],[],[]] #Nome - Prova 1 - Prova 2
+boletim = [[],[],[],[]] #Nome - Prova 1 - Prova 2
 
 while True:
     
@@ -15,13 +15,16 @@ while True:
     prova_um = float (input("Digite a nota da primeira prova: "))
     prova_dois = float (input("Digite a nota da segunda prova: "))
     
+    media = (prova_um+prova_dois)/2
+    
     boletim[0].append(nome)
     boletim[1].append(prova_um)
     boletim[2].append(prova_dois)
+    boletim[3].append(media)
     
     saida = str(input("Desaja continuar? [S/N] ")).lower()
     
-    if saida in "nnao":
+    if saida in ["n",'nao']:
         
         break
   
@@ -33,7 +36,7 @@ print("-"*30)
 
 for b in range(len(boletim[0])):
     
-    print(f"{b:<4}{boletim[0]:>10}{(boletim[1]+boletim[2])/2:>12}")
+    print(f"{b:<4}{boletim[0][b]:>10}{boletim[3][b]:>12}")
     
 print("="*30)
     
