7a9e28ce5e35196feb902d7b07d454644d3eb65c
PedroRockembach <rockpedro11@gmail.com>
2025-06-03 10:56:33 -0300

    ADIÇÃO DE METODO IMPLICITO REPEAT
    Neste commit está sendo adicionado um parametro a mais para a def sorteio lista,
    O parametro "repeat" visa possibilitar usar essa def em diversos programas podendo gerar sequenciais que repetem ou não

--- Desafios De Python/Desafio 100.py
+++ Desafios De Python/Desafio 100.py
@@ -2,15 +2,40 @@ from os import system
 
 
 system("cls")
+
 numeros = []
-def sorteio_lista(lista):
+
+def sorteio_lista(lista,repeat=True):
+    """
+    Recebe uma lista e preenche de valores entre 1 e 10 
+
+    Args:
+        lista (int): _description_
+        repeat (bool): Se True, valores podem se repetir; se False, valores serão únicos.
+    """
     from time import sleep
     from random import randint
-    for i in range(5):      
-        aleatorio = randint(1,10) 
-        lista.append(aleatorio)    
-        print (f'{aleatorio} ',end=' ',flush=True)
-        sleep(0.5)
+     
+    if repeat == True:
+        
+        for i in range(5):      
+            aleatorio = randint(1,10) 
+            lista.append(aleatorio)    
+            print (f'{aleatorio} ',end=' ',flush=True)
+            sleep(0.5)
+    if repeat == False:
+        
+        cont=0
+        while cont < 5:      
+            aleatorio = randint(1,10)
+            if aleatorio not in numeros:
+                numeros.append(aleatorio)
+                cont+=1    
+            else:
+                continue
+            print (f'{aleatorio} ',end=' ',flush=True)
+            sleep(0.5)
+        
  
 def soma_par(lista):   
     
@@ -21,5 +46,5 @@ def soma_par(lista):
             
     print(f"\nA soma dos numeros pares da lista: {lista} foi {pares}")
     
-sorteio_lista(numeros)
+sorteio_lista(numeros,repeat=False)
 soma_par(numeros)
