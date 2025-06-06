8d383ccf08a0664d37ae01442f737f80dff36184
PedroRockembach <rockpedro11@gmail.com>
2025-06-06 09:01:44 -0300

    umas defs e mudanças ai

---
+++ Desafios De Python/codewars.py
@@ -0,0 +1,13 @@
+first = 'word'
+
+'''lista = list(first)
+lista.append(lista[0])
+lista.pop(0)
+''.join(lista)
+saida = ''
+for i in range(len(first)):
+    saida += lista[i]'''
+    
+    
+saida = first[1:] + first[0] + 'ay'
+print(saida)
